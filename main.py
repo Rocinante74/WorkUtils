d28992129ea66f821c98b86331635cfdae9f7942
David Hood <dhood@ae-inc.com>
2023-01-27 08:51:11 -0600

    Major Refactor
    Add multiple path options

--- main.py
+++ main.py
@@ -1,93 +1,144 @@
 import os
 import shutil
 from datetime import datetime
+from typing import Dict, Any
+
 from watchfiles import run_process
 import time
 import csv
+import json
 
-# Open the CSV file with the `csv` module to know where to copy files
-with open('project_dict.csv') as csvfile:
-    # Create a reader object from the CSV file
-    reader = csv.reader(csvfile)
 
-    # Create an empty dictionary
-    destinations = {}
-
-    for row in reader:
-        # Iterate through the csv file and add a "My Drive" location for personal backup
-        # Each row should be in this format:
-        # JobNumber,DirectoryToCopyTo,SecondDirectoryToCopyTo
-
-        number = row[0]
-        dirs = row[1:]
-
-        # Adding the "My Drive" folder + ensuring it exists
-        my_drive_path = "G:\\My Drive\\Projects\\" + number
-        if not os.path.exists(my_drive_path):
-            os.mkdir(my_drive_path)
-        my_drive_path = my_drive_path + "\\Prints"
-        if not os.path.exists(my_drive_path):
-            os.mkdir(my_drive_path)
-        dirs.append(my_drive_path)
-
-        destinations[number] = dirs
-
-# The path to the folder that we want to watch
+# The path(s) to the folder(s) that we want to watch
 folder_to_watch = os.path.expanduser('~\Documents')
+second_folder = os.path.expanduser('~\Downloads')
 
 
-def run_script():
-    # Add some sleep time to ensure the file is fully present
-    time.sleep(5)
+class CopyRobot:
+    def __init__(self):
+        self.calc_destinations = self.get_calc_destinations()
+        self.plot_destinations = self.get_plot_destinations()
 
-    # get pdf files in the directory
-    pdf_files = [f for f in os.listdir(folder_to_watch) if f.endswith(".pdf")]
+    def go(self, change_list):
+        """
+        this is the function to run everything (the target for the run_process function)
+        :type change_list: list[list[str,str]]
+        """
+        time.sleep(5)
+        for change in change_list:
+            reason: str = change[0]
+            path: str = change[1]
 
-    # Loop through the PDF files
-    for pdf_file in pdf_files:
+            folder = path.split("\\")[-2]
+            file_name = path.split("\\")[-1]
 
-        # Ignore files with "DNC" to enable ignoring PDFs to be edited before copying
-        # Removing "DNC" will trigger another change in the folder, causing the file to be moved
-        if "DNC" in pdf_file:
-            continue
-
-        # Get the current timestamp
-        timestamp = datetime.now().strftime("%Y-%m-%d")
-
-        # Construct the new file name
-        new_file_name = timestamp + " - " + pdf_file
-
-        # Get the job number
-        job_number = ""
-        for job_numbers in destinations:
-            if job_numbers in pdf_file:
-                job_number = job_numbers
-                break
-        if not job_number:
-            continue
-
-        # Use the job number to determine where the PDFs should go
-        destination_folders = destinations[job_number]
-
-        # Check if the destination folders exist - Shouldn't hit this
-        for folder in destination_folders:
-            if not folder:
-                continue
-            if not os.path.exists(folder):
-                print("The destination folder '%s' does not exist. Please check the path and try again." % folder)
+            if reason == "deleted":
                 continue
 
-        # Copy the file to the destination folders
-        for folder in destination_folders:
+            if "DNC" in file_name:
+                continue
+
+            if folder == "Documents":
+                self.documents_workflow(path)
+            elif folder == "Downloads":
+                self.downloads_workflow(path)
+
+    def parse_dict_csv(self):
+        pass
+
+    def get_plot_destinations(self):
+        # Open the CSV file with the `csv` module to know where to copy files
+        with open('project_dict.csv') as csvfile:
+            # Create a reader object from the CSV file
+            reader = csv.reader(csvfile)
+
+            # Create an empty dictionary
+            plot_destinations = {}
+
+            for row in reader:
+                # Iterate through the csv file and add a "My Drive" location for personal backup
+                # Each row should be in this format:
+                # JobNumber,DirectoryToCopyTo,SecondDirectoryToCopyTo
+
+                number = row[0]
+                dirs = row[1:]
+
+                # Adding the "My Drive" folder + ensuring it exists
+                my_drive_path = "G:\\My Drive\\Projects\\" + number
+                if not os.path.exists(my_drive_path):
+                    os.mkdir(my_drive_path)
+                my_drive_path = my_drive_path + "\\Prints"
+                if not os.path.exists(my_drive_path):
+                    os.mkdir(my_drive_path)
+                dirs.append(my_drive_path)
+
+                plot_destinations[number] = dirs
+        return plot_destinations
+
+    def get_calc_destinations(self):
+        # Open the CSV file with the `csv` module to know where to copy files
+        with open('calc_dict.csv') as csvfile:
+            # Create a reader object from the CSV file
+            reader = csv.reader(csvfile)
+
+            # Create an empty dictionary
+            plot_destinations = {}
+
+            for row in reader:
+                # Iterate through the csv file and add a "My Drive" location for personal backup
+                # Each row should be in this format:
+                # JobNumber,DirectoryToCopyTo,SecondDirectoryToCopyTo
+
+                number = row[0]
+                dirs = row[1:]
+
+                # Adding the "My Drive" folder + ensuring it exists
+                my_drive_path = "G:\\My Drive\\Projects\\" + number
+                if not os.path.exists(my_drive_path):
+                    os.mkdir(my_drive_path)
+                my_drive_path = my_drive_path + "\\Calcs"
+                if not os.path.exists(my_drive_path):
+                    os.mkdir(my_drive_path)
+                dirs.append(my_drive_path)
+
+                plot_destinations[number] = dirs
+        return plot_destinations
+
+    def documents_workflow(self, full_path):
+        file_name = full_path.split("\\")[-1]
+
+        for job_number in self.plot_destinations.keys():
+            if job_number in file_name:
+                folders_to_copy = self.plot_destinations[job_number]
+                self.copy_files(full_path, folders_to_copy)
+
+    def downloads_workflow(self, full_path):
+        file_name = full_path.split("\\")[-1]
+
+        for job_number in self.calc_destinations.keys():
+            if job_number in file_name:
+                folders_to_copy = self.calc_destinations[job_number]
+                self.copy_files(full_path, folders_to_copy)
+
+    def copy_files(self, file, folders_to_copy):
+        for folder in folders_to_copy:
             if not folder:
                 continue
             try:
-                shutil.copy(os.path.join(folder_to_watch, pdf_file), os.path.join(folder, new_file_name))
+                shutil.copy(file, os.path.join(folder, self.get_new_name(file)))
+
+                # Delete the original file
+                os.remove(file)
             except:
                 print("Something went wrong copying to '%s'. Maybe close the file?" % folder)
 
-        # Delete the original file
-        os.remove(os.path.join(folder_to_watch, pdf_file))
+    def get_new_name(self, file_path):
+        file_name = file_path.split("\\")[-1]
+
+        timestamp = datetime.now().strftime("%Y-%m-%d")
+        new_file_name = timestamp + " - " + file_name
+
+        return new_file_name
 
 
 # Check if the watched folder exists first
@@ -95,5 +146,21 @@ if not os.path.exists(folder_to_watch):
     print("The folder to watch does not exist. Please check the path and try again.")
     exit()
 
+copy_bot = CopyRobot()
+
+
+def copy_process():
+    # changes will be an empty list "[]" the first time the function is called
+    # gives a list in the format [["reason 1 change", "path to file 1"], ["reason 2 change", "path to file 2"]]
+    changes = os.getenv('WATCHFILES_CHANGES')
+    changes = json.loads(changes)
+
+    if not changes:
+        # could use this for initial run
+        return
+
+    copy_bot.go(changes)
+
+
 if __name__ == '__main__':
-    run_process(folder_to_watch, target=run_script)
+    run_process(folder_to_watch, second_folder, target=copy_process)
